418d9758ee34014b425867b0002df15ab7081488
Khelthyr <cedric.nils.vincendeau@gmail.com>
2022-04-07 20:02:08 +0200

    added db

--- server/index.js
+++ server/index.js
@@ -1,6 +1,26 @@
 const express = require('express')
 const app = express()
 
+const mysql = require('mysql')
+
+const db = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'neo-randstad'
+})
+
+// Routes for the app
+app.get('/', (req, res) => {
+    const sqlInsert = ""
+    db.query(sqlInsert, (err, result) => {
+        res.send("hello world");
+    })
+});
+
 app.listen(3001, () => {
     console.log("Run on port 3001");
+    console.log("this is a test ");
 })
+
+module.exports = db;

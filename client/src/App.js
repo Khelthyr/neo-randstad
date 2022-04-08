b411aae5ee32b6fd8c074018e05445f10426d4fd
Khelthyr <cedric.nils.vincendeau@gmail.com>
2022-04-08 03:30:14 +0200

    Added Linkedin API Login Feature

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,99 @@
-import React, {Component} from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './App.css';
 
-// import Header from './components/Header';
-import Navbar from './components/Navbar';
-import Inscription from './pages/Inscription';
 
-function App() {
-  return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route exact path='/'/>
-        <Route path='/inscription' element={<Inscription/>}/>
-      </Routes>
-    </Router>
-  );
+import React from 'react';
+import{ LinkedInApi, NodeServer } from './config';
+import axios from 'axios';
+import linkedInLoginImage from './linkedin-login-images/Retina/Sign-In-Small---Default.png';
+
+export default class App extends React.Component {
+  initialState = {
+    user: {},
+    loggedIn: false
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = this.initialState;
+  }
+
+  componentDidMount = () => {
+    if (window.opener && window.opener !== window) {
+      const code = this.getCodeFromWindowURL(window.location.href);
+      window.opener.postMessage({'type': 'code', 'code': code}, '*')
+      window.close();
+    }
+      window.addEventListener('message', this.handlePostMessage);
+  };
+
+  handlePostMessage = event => {
+    console.log('event receveid')
+    if (event.data.type === 'code') {
+      const { code } = event.data;
+      this.getUserCredentials(code);
+    }
+  };
+
+  getCodeFromWindowURL = url => {
+    const popupWindowURL = new URL(url);
+    return popupWindowURL.searchParams.get("code");
+  };
+
+  showPopup = () => {
+    const { clientId, redirectUrl, oauthUrl, scope, state } = LinkedInApi;
+    const oauthUrl2 = `${oauthUrl}&client_id=${clientId}&scope=${scope}&state=${state}&redirect_uri=${redirectUrl}`;
+    const width = 450,
+      height = 730,
+      left = window.screen.width / 2 - width / 2,
+      top = window.screen.height / 2 - height / 2;
+    window.open(
+      oauthUrl2,
+      'Linkedin',
+      'menubar=no,location=no,resizable=no,scrollbars=no,status=no, width=' +
+      width +
+      ', height=' +
+      height +
+      ', top=' +
+      top +
+      ', left=' +
+      left
+    );
+  };
+
+  getUserCredentials = code => {
+    console.log(`${NodeServer.baseURL}${NodeServer.getUserCredentials}?code=${code}` )
+
+    axios
+      .get(`${NodeServer.baseURL}${NodeServer.getUserCredentials}?code=${code}`)
+      .then(res => {
+        const userRes = res.data.user;
+        this.setState({
+          user: userRes,
+          loggedIn: true
+        })
+        console.log("user : " + userRes)
+        // Do something with user
+      });
+  };
+
+  render() {
+    const { loggedIn, user } = this.state;
+    const contentWhenLoggedIn = (
+      <>
+        <img src={user.profileImageURL} alt="Profile image" />
+        <h3>{`${user.firstName} ${user.lastName}`}</h3>
+        <h3>{user.email}</h3>
+      </>
+    );
+    const contentWhenLoggedOut = (
+        <>
+          <h2>Sign in with LinkedIn</h2>
+          <img className="profilImage" src={linkedInLoginImage} alt="Sign in with LinkedIn"onClick={this.showPopup} />
+        </>
+    );
+    return (
+      <div>
+        {loggedIn && user !== {} ? contentWhenLoggedIn : contentWhenLoggedOut}
+      </div>
+    )
+  };
 }
-
-export default App;
--- server/index.js
+++ server/index.js
@@ -1,26 +1,118 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const axios = require('axios');
+const qs = require('query-string');
+app.use(cors());
 
-const mysql = require('mysql')
+// Constantes
+const urlToGetLinkedInAccessToken = 'https://www.linkedin.com/oauth/v2/accessToken';
+const urlToGetUserProfile ='https://api.linkedin.com/v2/me?projection=(id,localizedFirstName,localizedLastName,profilePicture(displayImage~digitalmediaAsset:playableStreams))'
+const urlToGetUserEmail = 'https://api.linkedin.com/v2/clientAwareMemberHandles?q=members&projection=(elements*(primary,type,handle~))';
 
-const db = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'neo-randstad'
+app.get('/getUserCredentials', function (req, res) {
+    console.log('Starts here');
+  const user = {};
+  const code = req.query.code;
+
+    getAccessToken(code)
+    .then((response) => {
+        getUserProfile(response.data["access_token"])
+        .then((response) => {
+            resStatus = 200;
+            let user = {};
+            user.firstName = response.data["localizedFirstName"];
+            user.lastName = response.data["localizedLastName"];
+            user.profileImageURL = response.data.profilePicture["displayImage~"].elements[0].identifiers[0].identifier;
+            res.status(resStatus).json({ user })
+        })
+    }).catch((error) => {
+        res.status(500).send(`${error}`)
+        console.error(error)
+      })
+
 })
 
-// Routes for the app
-app.get('/', (req, res) => {
-    const sqlInsert = ""
-    db.query(sqlInsert, (err, result) => {
-        res.send("hello world");
+/**
+ * Get access token from LinkedIn
+ * @param code returned from step 1
+ * @returns accessToken if successful or null if request fails 
+ */
+function getAccessToken(code) {
+  let accessToken = null;
+  const config = {
+    headers: { "Content-Type": 'application/x-www-form-urlencoded' }
+  };
+  const parameters = {
+    "grant_type": "authorization_code",
+    "code": code,
+    "redirect_uri": "http://localhost:3000/",
+    "client_id": "8611igirv8nck4",
+    "client_secret": "EIJ01cYdrG9GpGTM",
+  };
+  return axios
+    .post(
+        urlToGetLinkedInAccessToken,
+      qs.stringify(parameters),
+      config)
+    .catch(err => {
+      console.log("Error getting LinkedIn access token : " + err );
     })
+    // return accessToken;
+}
+
+/**
+ * Get user first and last name and profile image URL
+ * @param accessToken returned from step 2
+ */
+function getUserProfile(accessToken) {
+  let userProfile = null;
+  const config = {
+    headers: {
+      "Authorization": 'Bearer ' + accessToken 
+    }
+  }
+  console.log("parmaters : " + qs.stringify(config))
+  console.log("access : " + accessToken);
+  return axios
+    .get(urlToGetUserProfile, config)
+    .catch(error => console.log("Error grabbing user profile : " + error))
+}
+
+
+/**
+ * Get user email
+ * @param accessToken returned from step 2
+ */
+function getUserEmail(accessToken) {
+  const email = null;
+  const config = {
+    headers: {
+      "Authorization": 'Bearer ' + accessToken
+    }
+  };
+  return axios
+    .get(urlToGetUserEmail, config)
+    // .then(response => {
+    //   email = response.data.elements[0]["handle~"];
+    // })
+    .catch(error => console.log("Error getting user email"))
+
+  return email;
+}
+
+/**
+ * Build User object
+ */
+function userBuilder(userProfile, userEmail) {
+  return {
+    firstName: userProfile.firstName,
+    lastName: userProfile.lastName,
+    profileImageURL: userProfile.profileImageURL,
+    email: userEmail
+  }
+}
+
+app.listen(3001, function () {
+  console.log(`Node server running...`)
 });
-
-app.listen(3001, () => {
-    console.log("Run on port 3001");
-    console.log("this is a test ");
-})
-
-module.exports = db;
